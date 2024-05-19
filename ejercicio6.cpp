c649e42077603b62f0439f59f5bd953eb6ba02cd
unknown <jkalicha@gmail.com>
2024-05-19 17:07:31 -0300

    ej6

---
+++ ejercicio6.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+
+/*void kruskal() ​
+ColaPrioridad cp(); // imp. heap​
+insertarAristas(cp); // inserto todas las aristas del grafo​
+List<Arista> solucion(); // guardo las aristas que serán parte de la solución​
+MFset mfset(V+1); // Merge-Find Set de V+1 elementos​
+int aristasAceptadas = 0;​
+​
+while(!cp.estaVacia() || aristasAceptadas < V-1)​
+Arista a = cp.pop(); // obtenemos la misma arista sin procesar​
+if(mfset.find(a.origen) != mfset.find(a.destino)) // !formaCiclo(solucion, a)​
+mfset.merge(a.origen, a.destino);​
+solucion.agregar(a);​
+aristasAceptadas++;
+*/
+
+int main()
+{
+
+    return 0;
+}
