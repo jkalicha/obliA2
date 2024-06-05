cf65f53cb4217bec54f2a9cca8acd1b2caf27890
unknown <JC314750@fi365.ort.edu.uy>
2024-06-05 15:50:37 -0300

    ejercicio9

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,75 +1,92 @@
-#include <cassert>
-#include <string>
 #include <iostream>
-#include <limits>
 #include <cmath>
 
 using namespace std;
 
-    int maximo(int x, int y){
-        if (x > y){
-            return x;
-        }
-        return y;
+// Función para obtener el máximo de dos números
+int maximo(int x, int y) {
+    return (x > y) ? x : y;
+}
+
+// Función para generar números primos usando la criba de Eratóstenes
+int* esPrimoEratostenes(int n, int& cant) {
+    int* numeros = new int[n + 1]();
+    for (int i = 0; i <= n; ++i) {
+        numeros[i] = 1;
     }
-    //preguntar si esta bien la logica de la funcion esPrimoEratostenes
-    int* esPrimoEratostenes (int n){
-        int * numeros = new int[n+1]();
-        for (int i = 0; i < n+1; i++){
-            numeros[i] = i;
+    numeros[0] = numeros[1] = 0;
+
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (numeros[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                numeros[j] = 0;
+            }
         }
-        int cant = n-1;
-        for (int i = 2; i <= sqrt(n); i++){
-            if (numeros[i] != -1){
-                for (int j = i; j<(n/i); j++){
-                    numeros[i*j] = -1;
-                    cant--;
+    }
+
+    cant = 0;
+    for (int i = 2; i <= n; ++i) {
+        if (numeros[i]) {
+            ++cant;
+        }
+    }
+
+    int* primos = new int[cant]();
+    int index = 0;
+    for (int i = 2; i <= n; ++i) {
+        if (numeros[i]) {
+            primos[index++] = i;
+        }
+    }
+
+    delete[] numeros;
+    return primos;
+}
+
+int main() {
+    int filas, columnas;
+    cin >> filas;
+    cin >> columnas;
+
+    // Generamos los números primos necesarios
+    int cant;
+    int* primos = esPrimoEratostenes(maximo(filas, columnas), cant);
+
+    // Inicializamos la matriz con ceros
+    int** matriz = new int*[filas];
+    for (int i = 0; i < filas; ++i) {
+        matriz[i] = new int[columnas]();
+    }
+
+    // Establecemos las condiciones iniciales
+    matriz[0][0] = 1;
+    matriz[1][0] = 0;
+    matriz[0][1] = 0;
+
+    // Llenamos la matriz
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            for (int k = 0; k < cant; ++k) {
+                int primo = primos[k];
+                if (i - primo >= 0) {
+                    matriz[i][j] += matriz[i - primo][j];
+                }
+                if (j - primo >= 0) {
+                    matriz[i][j] += matriz[i][j - primo];
                 }
             }
         }
-        int* primos = new int[cant]();
-        int j = 0;
-        for (int i = 2; i < n+1 ; i++){
-            if (numeros[i] != -1){
-                primos[j] = i;
-                j++;
-            }
-        }
-        return primos;
     }
 
-    //preguntar como resolver el ejercicio despues de obtener los primos.
-    //como obtener todas las combinaciones posibles de llegar de una esquina a la otra
-    //acordarse de usar long long
+    // Mostramos el resultado
+    cout << matriz[filas - 1][columnas - 1] << endl;
 
-int main()
-{
-    int x;
-    int y;	
-    cin >> x;
-    cin >> y;
-    //encontramos los primos
-    int* primosFila = esPrimoEratostenes(x);
-    int* primosCol = esPrimoEratostenes(y);
-    //inicializamos la matriz
-    int ** mat = new int*[y];
-    for (int i=0; i<y; i++){
-        mat[i] = new int[x];
+    // Liberamos la memoria
+    for (int i = 0; i < filas; ++i) {
+        delete[] matriz[i];
     }
-    int actual = 0;
-    //dibujamos la matriz
-    for (int i = 0; i<x; i++){
-        for (int j=0; j<y; j++){
-            if (j == 0 && i == 0){
-                mat[i][j] = 1;
-            }
-            if (i-primosFila[actual]>=0){
-                mat[i][j] += mat[i-primosFila[actual]]; 
-            }
-            actual++;
-
-        }
-    }
+    delete[] matriz;
+    delete[] primos;
 
     return 0;
 }
